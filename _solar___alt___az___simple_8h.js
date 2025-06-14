79fc7b4d31ff1e7f13c9e1ec8fdeee1cda473809
shivamkundan <shivamkundan@users.noreply.github.com>
2025-06-14 04:57:09 +0000

    deploy: 29dff254d3d0914456a9cc005c6e38eac9c3fa03

---
+++ _solar___alt___az___simple_8h.js
@@ -0,0 +1,10 @@
+var _solar___alt___az___simple_8h =
+[
+    [ "computeSolar", "_solar___alt___az___simple_8h.html#aba7d692392288f2265613b84f597bcf1", null ],
+    [ "dayOfYear", "_solar___alt___az___simple_8h.html#a542d023432412dd8ab12fa7f609c5b63", null ],
+    [ "getDeclination", "_solar___alt___az___simple_8h.html#a2e1a1f2d13021b7daac14c84993bcb74", null ],
+    [ "getGamma", "_solar___alt___az___simple_8h.html#a2681d780b255b4841887a1e3b6a16329", null ],
+    [ "getHourAngle", "_solar___alt___az___simple_8h.html#af754d64862fa0d97ddc0c7648d0dcc17", null ],
+    [ "getIrradiance", "_solar___alt___az___simple_8h.html#a0db407e07b22ad85c0fd4b1adfc36ee9", null ],
+    [ "isDST", "_solar___alt___az___simple_8h.html#ac9779eef9a50327860993b2c5ad0c21f", null ]
+];
---
+++ files_dup.js
@@ -0,0 +1,5 @@
+var files_dup =
+[
+    [ "Solar_Alt_Az_Simple.h", "Solar__Alt__Az__Simple_8h.html", "Solar__Alt__Az__Simple_8h" ],
+    [ "Solar_Alt_Az_Simple.ino", "Solar__Alt__Az__Simple_8ino.html", null ]
+];
